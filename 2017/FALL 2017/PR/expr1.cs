9de50ef293f1b770c814d493eefb8868f156a488
iShamSLam <31825566+iShamSLam@users.noreply.github.com>
2017-09-14 13:11:24 +0300

    Добавленый файлы семинара

---
+++ 2017/FALL 2017/PR/expr1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace expr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = 5;
+            b = 10;
+            b = b - a;
+            a = a + b;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
---
+++ 2017/FALL 2017/PR/expr2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            int c = a;
+        
+            int b = c / 4 * 10;
+            int e = c / 6;
+            int d = c / 120;
+            int s = b + e + d;
+            Console.WriteLine(a);
+            Console.WriteLine(s);
+        }
+    }
+}
+ 
