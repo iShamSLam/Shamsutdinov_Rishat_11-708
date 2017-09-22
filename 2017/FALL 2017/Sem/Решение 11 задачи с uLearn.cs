bee0ff512a305ce8a367a0184f2dd72bb78bbaf4
iShamSLam <31825566+iShamSLam@users.noreply.github.com>
2017-09-22 21:41:31 +0300

    [add] Решение 11 задачи блока ошибки с Ulearn

---
+++ 2017/FALL 2017/Sem/Решение 11 задачи с uLearn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_11_Ulearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Кол-во полных часов ");           
+            int hours = int.Parse(Console.ReadLine());
+            Console.WriteLine("Кол-во полных минут ");
+            int min = int.Parse(Console.ReadLine());
+            double minCursorStep = min * 6;
+            double angleBetweenCursores = (hours + (min / 60)) * 30 - minCursorStep;
+            Console.WriteLine("Разница между минутной и часовой стрелкой равна == " + angleBetweenCursores + "  gradusov " );
+            Console.ReadLine();
+        }
+    }
+}
