298ec745c29b4ca7ab4de9c5b52c9ed9e1960056
iShamSLam <31825566+iShamSLam@users.noreply.github.com>
2018-02-21 22:02:57 +0300

    [add]Алгоритм лексикографической сортировки

---
+++ 2018/Spring/Algorithms_and_Data_Structures/SemM/Lexographical_Sort.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASY_Sort
+{
+    class LexicographicalSort
+    {
+        private string word;
+        private List<string> sampleList = new List<string>();
+        public void Sort()
+        {
+            Console.WriteLine("Начинается считывание слов, если хотите прекратить вбейте \"end\"");
+            do
+            {
+                if (word != "end")
+                {
+                    word = Console.ReadLine();
+                    sampleList.Add(word);
+                }
+            }
+            while (word!="end");
+            foreach(var s in SortByLenght(sampleList))
+            {
+                Console.WriteLine("....{0}",s);
+            }
+            Console.ReadLine();
+        }
+
+        static IEnumerable<string> SortByLenght(IEnumerable<string> e)
+        {
+            var sorted = e.OrderBy(s => s).ThenBy(r=>r.Length);
+            return sorted;
+        }
+  
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rema = new LexicographicalSort();
+            rema.Sort();
+        }
+    }
+}
