2e1c2e0fbf35bdae21f86c337af17d28cb3a3559
iShamSLam <31825566+iShamSLam@users.noreply.github.com>
2018-02-27 15:12:47 +0300

    [add]Эмбрион задачи на связность

---
+++ 2018/Spring/Algorithms_and_Data_Structures/Misk/Somethin_Ropery_One_By_Another.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Somethings_Ropery_Both_By_Both
+{
+    class Ropery
+    {
+        // используется двумерный массив для обозначения связности,
+        private int[,] Numbers_Array = new int[,] {/*put something into it */};
+        
+        public int[,] Ropery_Find(int p)
+        {
+            for(int i=0; i < Numbers_Array.Length;i++)
+            {
+              // To do...
+            }
+
+
+            return(Numbers_Array);
+        }
+        public int[,] Union()
+        {
+            // To do...
+            return (Numbers_Array);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // checking...
+        }
+    }
+}
