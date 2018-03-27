9c96d4c592f62211df2a72e4e733a6e8cb9a91ab
iShamSLam <31825566+iShamSLam@users.noreply.github.com>
2018-03-27 14:31:06 +0300

    [add] Linq Exersise done at seminar

---
+++ 2018/Spring/Computer_Science/Misk/Linq First Block 4th exersise.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Ex__4___1_
+{
+    public class AbadonedClass
+    {
+        public static List<string> Sorter(IEnumerable<long> words)
+        {
+            return words
+                .Where(word => word % 2 != 0)
+                .Select(word => word.ToString())
+                .OrderBy(word => word.Length)
+                .ThenBy(word => word)
+                .ToList();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            var words = new List<long>();
+            for (int i = 0; i < 200; i++)
+            {
+                words.Add(rand.Next(1, 255));
+            }
+                foreach (var item in AbadonedClass.Sorter(words))
+                Console.WriteLine(item);
+        }
+    }
+}
