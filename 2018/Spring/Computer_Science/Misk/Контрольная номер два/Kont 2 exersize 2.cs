dc276bb67013b6e9a8ff70a2e42ff4c6747f1a5c
iShamSLam <31825566+iShamSLam@users.noreply.github.com>
2018-05-28 13:40:11 +0300

    [add] Вторая контрольная

---
+++ 2018/Spring/Computer_Science/Misk/Контрольная номер два/Kont 2 exersize 2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Kontr2_Ex_3
+{
+    /// <summary>
+    /// Класс для того чтобы преобразовать json формат в него
+    /// </summary>
+    class Post
+    {
+        public int postId { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string body { get; set; }
+    }
+    class Control_Work
+    {
+        public void DowloadAndJsoning(string url)
+        {
+            WebClient client = new WebClient();
+            // папка будет в bin/debug и там будет ссылка на сайт с которого скачивались данные 
+            string directory = new Uri(url).Host;
+            Directory.CreateDirectory(directory);
+            // скачивание джэйсона в формате строки
+            var e = client.DownloadString(new Uri(url));
+            // преобразование джэйсон строк в обьекты класса
+            Post[] posts = JsonConvert.DeserializeObject<Post[]>(e);
+            var res = posts.Where(post => post.id % 2 == 0);
+            foreach (var cl in res)
+            {
+                // промежуточный результат
+                 int semiresult=0;
+                var meas = cl.body.Split(' ');
+                foreach(var word in meas)
+                {
+                    semiresult += word.Length;
+                }
+                Console.WriteLine("Длина комментария {0} равна : {1} букв", cl.id, semiresult);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new Control_Work();
+            test.DowloadAndJsoning("https://jsonplaceholder.typicode.com/comments?postId=1");
+        }
+    }
+}
---
+++ 2018/Spring/Computer_Science/Misk/Контрольная номер два/Kontr 2 Multi.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+
+namespace Kontr2_1_exersize
+{
+    // Вспомогательный класс соответсвующий джэйсон представлению
+    class OnceFile
+    {
+        public int albumId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public Uri url { get; set; }
+        public Uri thumbnailUrl { get; set; }
+    }
+    class Control_Work
+    {
+        public void DowloadAndJsoning(string url)
+        {
+            WebClient client = new WebClient();
+            // файл проекта бин дебаг там папка с урл ссылкой там будут все файлы
+            string directory = new Uri(url).Host;
+            Directory.CreateDirectory(directory);
+            // скачивание джэйсон представления в формате строки
+            var e = client.DownloadString(new Uri(url));
+            // преобразование джэйсона в обьекты вспом класса
+            OnceFile[] inform = JsonConvert.DeserializeObject<OnceFile[]>(e);
+            foreach (var inf in inform)
+            {
+                // скачивание файликов по ссылкам
+              client.DownloadFile(inf.thumbnailUrl, Path.Combine(directory,String.Format("Album: {0} Song: {1}", inf.albumId, inf.id)));
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new Control_Work();
+            test.DowloadAndJsoning("https://jsonplaceholder.typicode.com/photos");
+        }
+    }
+}
